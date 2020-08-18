cfccea4b76fad407fef00e90db6bde3f69cdc8e0
Paulo Borges <pborges086@hotmail.com>
2020-08-17 21:07:34 -0300

    Adicionando tipo generico para StreamController

--- lib/util/simple_bloc.dart
+++ lib/util/simple_bloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 class SimpleBloc<T>{
   
-  final _streamController = StreamController();
+  final _streamController = StreamController<T>();
 
   get stream => _streamController.stream;
 
